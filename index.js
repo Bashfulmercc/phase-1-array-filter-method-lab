eccb5c0e040232277f00276d332f276c55cb78da
Mason <swartmason@gmail.com>
2023-03-27 14:22:37 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, name){
+    return array.filter((el) => el.toLowerCase().includes(name.toLowerCase()));
+}
+
+function fuzzyMatch(array, name){
+    return array.filter((el) => el.charAt(0).includes(name.charAt(0)));
+}
+
+function matchName(array, name){
+    return array.filter((obj) => obj.name.includes(name));
+}
